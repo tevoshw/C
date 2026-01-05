20603f14f0409dadcb60689ca32694cebc78eb7f
tevoshw <tevoshw@gmail.com>
2026-01-05 13:01:38 -0300

    Add convertor tips in C

---
+++ basics/convertor-primitive-tips/geral-convertor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+
+    // INT TO FLOAT
+
+    int n = 10;
+    float n1 = (float) n;
+    printf("%f\n", n1);
+
+
+    // FLOAT TO INT
+    float n2 = 10.5;
+    int n3 = (int) n2;
+    printf("%i\n", n3);
+
+    // TIP NEW-VARIABLE = (NEW TIPE) OLD-VARIABLE                -> FOR EVER CASE
+
+    return 0;
+}
