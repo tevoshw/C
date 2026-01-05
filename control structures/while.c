a12784d3dd22d3dfabde6a1efb3d2a5003ee4374
tevoshw <tevoshw@gmail.com>
2026-01-05 16:09:12 -0300

    Add CONTROL STRUCTURE WHILE in C

---
+++ control structures/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+    int n1 = 10;
+    int n2 = 0;
+
+    while (n1 > n2)
+    {
+        printf("O número atual é: %i \n", n2);
+        n2 += 1;
+    }
+
+    return 0;
+}
