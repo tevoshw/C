8b4a7ce1389a7a92c2f3e12637ddd832c9f0723c
tevoshw <tevoshw@gmail.com>
2026-01-13 14:43:38 -0300

    add ARRAY INICIALIZE in C

---
+++ data structures/arrays1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+
+    // INCIALIZE AN ARRAY 
+    // TYPE <NAME> [SIZE] = {X,Y,Z};
+
+    int numeros[3] = {1,2,3};
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", numeros[i]);
+    }
+    
+
+
+    return 0;
+}
